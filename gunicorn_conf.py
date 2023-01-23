2c7847b451ffdbb100793be595a98e0cb94a430d
Yanick Schraner <yanick.schraner@fhnw.ch>
2023-01-23 13:37:51 +0100

    Fix URLs

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -3,5 +3,5 @@ import multiprocessing
 bind = "127.0.0.1:80"
 # 1 worke need approx 3 GB of memory
 # The mem_limit: is set to 8G in the docker-compose
-workers = 2
+workers = 1
 # workers = multiprocessing.cpu_count() * 2 + 1

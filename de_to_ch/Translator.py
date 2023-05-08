3baa4d31e9e4811c9176747c9c8d6d8a6330cd09
Claudio Paonessa <clpao96@hotmail.com>
2023-05-08 10:31:43 +0200

    replace tokenizer with fast version and inserted torch inference mode for tokenizer and translation inference

--- de_to_ch/Translator.py
+++ de_to_ch/Translator.py
@@ -1,9 +1,10 @@
 import os
+import torch
 
 from de_to_ch.utils import setup_device
 
 from transformers.models.t5.modeling_t5 import T5ForConditionalGeneration
-from transformers.models.t5.tokenization_t5 import T5Tokenizer
+from transformers.models.t5.tokenization_t5_fast import T5TokenizerFast
 
 
 class Translator:
@@ -14,25 +15,27 @@ class Translator:
         self.device, n_gpu = setup_device()
 
         self.model = T5ForConditionalGeneration.from_pretrained(os.path.join(self.model_output_dir, 'best-model'))
-        self.tokenizer = T5Tokenizer.from_pretrained(self.t5)
+        self.tokenizer = T5TokenizerFast.from_pretrained(self.t5)
         self.model.to(self.device)
 
     def translate(self, sentences):
-        input_batch_pt = self.tokenizer(
-            sentences,
-            return_tensors='pt',
-            truncation=True,
-            max_length=256,
-            padding=True
-        )
-        decoded_out = self.model.generate(
-            input_batch_pt['input_ids'].to(self.device),
-            max_length=256,
-            pad_token_id=self.tokenizer.eos_token_id,
-            num_beams=self.beam_size,
-            num_return_sequences=1
-        )
-        pred_batch = self.tokenizer.batch_decode(decoded_out, skip_special_tokens=True)
+        with torch.inference_mode():
+            input_batch_pt = self.tokenizer(
+                sentences,
+                return_tensors='pt',
+                truncation=True,
+                max_length=256,
+                padding=True
+            )
+            decoded_out = self.model.generate(
+                input_batch_pt['input_ids'].to(self.device),
+                max_length=256,
+                pad_token_id=self.tokenizer.eos_token_id,
+                num_beams=self.beam_size,
+                num_return_sequences=1
+            )
+            pred_batch = self.tokenizer.batch_decode(decoded_out, skip_special_tokens=True)
+        
         return pred_batch
 
     def translate_one(self, sentence: str):
